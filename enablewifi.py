ae7506c240c513e154dc24eaba7f10d53e6cc90a
anfractuosity <anfractuosity@anfractuosity.com>
2022-06-06 22:22:36 +0100

    Command line argument for MAC

--- enablewifi.py
+++ enablewifi.py
@@ -1,6 +1,12 @@
+import argparse
 from bluepy.btle import UUID, Peripheral
 
-p = Peripheral("MAC","public")
-serv = p.getServiceByUUID("0000ffe0-0000-1000-8000-00805f9b34fb")
-c = serv.getCharacteristics()[0]
-c.write(bytes('GPIO3','utf-8'))
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Ceyomur wifi enabler")
+    parser.add_argument("--addr", dest="addr", type=str, help="Address of trail camera", required=True)
+
+    args = parser.parse_args()
+    p = Peripheral(args.addr, "public")
+    serv = p.getServiceByUUID("0000ffe0-0000-1000-8000-00805f9b34fb")
+    c = serv.getCharacteristics()[0]
+    c.write(bytes('GPIO3','utf-8'))
